eb6aa5c5cd34af281b209cddd84b90d794a6111d
LydiaSakovich <57660702+LydiaSakovich@users.noreply.github.com>
2019-12-21 23:44:44 +0300

    Add files via upload

--- jquery/rabota5.js
+++ jquery/rabota5.js
@@ -1,15 +1,13 @@
 $(".first_task").css("fontWeight", "bold");
 $(".first_task").css("color", "#112233");
 $(".first_task").css("fontSize", "15px");
-$(document).ready(function()
-{
+$(document).ready(function () {
 	$("input").attr("disabled", "disabled");
 });
 $("a").prepend("↗");
 $("a").attr("target", "_blank");
 
-function reloadPage() 
-{
+function reloadPage() {
 	let p = document.createElement("p");
 	$(p).load("https://inxaoc.github.io/example/ajax-1.html");
 	document.body.appendChild(p);
@@ -21,56 +19,54 @@ $(".btnInTable").css("width", "100%")
 $(".prgInTable").css("fontWeight", "bold");
 $(".prgInTable").css("fontSize", "15px");
 
-function toggle(i) 
-{
+function toggle(i) {
 	$(".prgInTable").eq(i).toggle(1000);
 }
 
-function fadeToggle(i) 
-{
+function fadeToggle(i) {
 	$(".prgInTable").eq(i).fadeToggle(1000);
 }
 
-function animateFont(i) 
-{
-	$(".prgInTable").eq(i).animate({fontSize: "20px"});
-	$(".prgInTable").eq(i).animate({fontSize: "15px"});
+function animateFont(i) {
+	$(".prgInTable").eq(i).animate({
+		fontSize: "20px"
+	});
+	$(".prgInTable").eq(i).animate({
+		fontSize: "15px"
+	});
 }
 
-function slideToggle(i) 
-{
+function slideToggle(i) {
 	$(".prgInTable").eq(i).slideToggle(1000);
 }
 
-function opacityAnimation(i) 
-{
-	$(".prgInTable").eq(i).animate({opacity: "0"}, 1000);
-	$(".prgInTable").eq(i).animate({opacity: "1"}, 1000);
+function opacityAnimation(i) {
+	$(".prgInTable").eq(i).animate({
+		opacity: "0"
+	}, 1000);
+	$(".prgInTable").eq(i).animate({
+		opacity: "1"
+	}, 1000);
 }
 
-function getJSON()
-{
-	$.getJSON("https://inxaoc.github.io/example/ajax.json", function(data) 
-	{
+function getJSON() {
+	$.getJSON("https://inxaoc.github.io/example/ajax.json", function (data) {
 		let ulMain = document.createElement("ul");
-		function jsonToList(obj,curUl) 
-		{
-			for (let key in obj) 
-			{
+
+		function jsonToList(obj, curUl) {
+			for (let key in obj) {
 				let li = document.createElement("li");
 				let item = key + " : ";
 				li.innerHTML = item;
 				curUl.appendChild(li);
-				if (typeof obj[key] == "object" && !Array.isArray(obj[key])) 
-				{
+				if (typeof obj[key] == "object" && !Array.isArray(obj[key])) {
 					let ul = document.createElement("ul");
-					jsonToList(obj[key],ul);
+					jsonToList(obj[key], ul);
 					curUl.appendChild(ul);
-				} else 
-				{
+				} else {
 					item += obj[key];
 					li.innerHTML = item;
-				}	
+				}
 			}
 		}
 		jsonToList(data, ulMain);
